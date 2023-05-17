1943ebbf25414f2f10f5e5a42e7e9f4ed543be35
405267330@qq.com <405267330@qq.com>
2023-05-17 17:24:13 +0800

    delete repo attr

--- bin/repo.js
+++ bin/repo.js
@@ -2,7 +2,6 @@
 export default {
     "py-react": {
         url: "direct:http://192.168.101.96:8090/pointcloud_frontend/project_template.git",
-        bootstrap: "npm run start",
     },
     // "py-vue": {
     //     url: "../templates/py-vue",
